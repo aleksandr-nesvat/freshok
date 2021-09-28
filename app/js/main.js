c57e5758feb9624e7f61a13dd09e6307d624fddb
aleksandr.nesvat <aleksandr.nesvat@gmail.com>
2021-09-28 16:38:29 +0300

    12 dz

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
   
+$('.slider__inner').slick({
+  nextArrow: '<button type="button" class="slick-next"><img src="images/slider/arrow_right.svg"></button>',
+  prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/arrow_left.svg"></button>',
+  fade: true
+});
+
+
+var mixer = mixitup('.goods__items');
+
+
   });
